1869badcb461e65a8f528dbf1865cb463e29bd66
unknown <vincentwoodward4@gmail.com>
2021-04-23 13:16:59 -0500

    Correctly breaks down instructions

--- legv8dis/src/com/company/Instruction.java
+++ legv8dis/src/com/company/Instruction.java
@@ -12,55 +12,53 @@ public class Instruction {
         this.instructionType = instructionType;
     }
 
-    public boolean opcodeMatch(String op) {
+    public boolean doesOpcodeMatch(String op) {
         return op.compareTo(opcode) == 0;
+    }
+
+    public String getOpcode(){
+        return opcode;
+    }
+
+    public String getInstructionName(){
+        return instructionStr;
     }
 
     public String[] breakInstruction(String i) {
         String[] brokenDown = new String[5];
 
         switch (instructionType) {
-            case "R": //R: Rm 5 bits, shamt 6 bits, Rn 5 bits, Rd 5 bits
+            case "R": //R: Opcode 11 bits, Rm 5 bits, shamt 6 bits, Rn 5 bits, Rd 5 bits
                 brokenDown[0] = i.substring(0, 11); //Opcode 11 bits
-                brokenDown[1] = i.substring(11, 17); //Rm 5 bits
-                brokenDown[2] = i.substring(17, 24); //Shamt 6 bits
-                brokenDown[3] = i.substring(24, 30); //Rn 5 bits
-                brokenDown[4] = i.substring(30, 35); //Rd 5 bits
+                brokenDown[1] = i.substring(11, 16); //Rm 5 bits
+                brokenDown[2] = i.substring(16, 22); //Shamt 6 bits
+                brokenDown[3] = i.substring(22, 27); //Rn 5 bits 22,23,24,25,26,27,28,29,30,31
+                brokenDown[4] = i.substring(27); //Rd 5 bits
                 break;
-            case "I": //I: immediate 11 bits, Rn 5 bits, Rd 5 bits
-                brokenDown[0] = i.substring(0, 11); //Opcode 11 bits
-                brokenDown[1] = i.substring(11, 17); //Immediate 11 bits
-                brokenDown[2] = i.substring(17, 24); //Rn 5 bits
-                brokenDown[3] = i.substring(24, 30); //Rd 5 bits
-                brokenDown[4] = i.substring(30, 35); //Null
+            case "I": //I: Opcode 10 bits, immediate 12 bits, Rn 5 bits, Rd 5 bits
+                brokenDown[0] = i.substring(0, 10); //Opcode 10 bits
+                brokenDown[1] = i.substring(10, 22); //Immediate 11 bits
+                brokenDown[2] = i.substring(22, 27); //Rn 5 bits
+                brokenDown[3] = i.substring(27); //Rd 5 bits
                 break;
-            case "D": //D: DT_address 9 bits, op 2 bits, Rn 5 bits, Rt 5 bits
+            case "D": //D: Opcode 11 bits, DT_address 9 bits, op 2 bits, Rn 5 bits, Rt 5 bits
                 brokenDown[0] = i.substring(0, 11); //Opcode 11 bits
-                brokenDown[1] = i.substring(11, 17); //DT_address 9 bits
-                brokenDown[2] = i.substring(17, 24); //op 2 bits
-                brokenDown[3] = i.substring(24, 30); //Rn 5 bits
-                brokenDown[4] = i.substring(30, 35); //Rt 5 bits
+                brokenDown[1] = i.substring(11, 20); //DT_address 9 bits
+                brokenDown[2] = i.substring(20, 22); //op 2 bits
+                brokenDown[3] = i.substring(22, 27); //Rn 5 bits
+                brokenDown[4] = i.substring(27); //Rt 5 bits
                 break;
-            case "B": //B: BR_address 26 bits
-                brokenDown[0] = i.substring(0, 11); //Opcode 11 bits
-                brokenDown[1] = i.substring(11, 17); //BR_address 26 bits
-                brokenDown[2] = i.substring(17, 24); //Null
-                brokenDown[3] = i.substring(24, 30); //Null
-                brokenDown[4] = i.substring(30, 35); //Null
+            case "B": //B: Opcode 6 bits, BR_address 26 bits
+                brokenDown[0] = i.substring(0, 6); //Opcode 6 bits
+                brokenDown[1] = i.substring(6); //BR_address 26 bits
                 break;
-            case "CB": //COND_BR_address 19 bits, Rt 5 bits
-                brokenDown[0] = i.substring(0, 11); //Opcode 11 bits
-                brokenDown[1] = i.substring(11, 17); //COND_BR_address 19 bits
-                brokenDown[2] = i.substring(17, 24); //Rt 5 bits
-                brokenDown[3] = i.substring(24, 30); //Null
-                brokenDown[4] = i.substring(30, 35); //Null
+            case "CB": //CB: Opcode 8 bits, COND_BR_address 19 bits, Rt 5 bits
+                brokenDown[0] = i.substring(0, 8); //Opcode 8 bits
+                brokenDown[1] = i.substring(8, 27); //COND_BR_address 19 bits
+                brokenDown[2] = i.substring(27); //Rt 5 bits
                 break;
-            case "IW": //IW: MOV_immediate 16 bits, Rd 5 bits
-                brokenDown[0] = i.substring(0, 11); //Opcode 11 bits
-                brokenDown[1] = i.substring(11, 17); //MOV_immediate 16 bits
-                brokenDown[2] = i.substring(17, 24); //Rd 5 bits
-                brokenDown[3] = i.substring(24, 30); //Null
-                brokenDown[4] = i.substring(30, 35); //Null
+            case "IW": //IW: Opcode 11 bits, IW: MOV_immediate 16 bits, Rd 5 bits
+                //IW not supported
                 break;
             default:
                 break;
--- legv8dis/src/com/company/Main.java
+++ legv8dis/src/com/company/Main.java
@@ -9,108 +9,153 @@ public class Main {
     public static void main(String[] args) {
 
          Instruction[] instructionSet = {
-                new Instruction("ADD", "0b10001011000", "R"),
-                new Instruction("ADDI", "0b1001000100", "I"),
-                new Instruction("ADDIS", "0b1011000100", "I"),
-                new Instruction("ADDS", "0b10101011000", "R"),
-                new Instruction("AND", "0b10001010000", "R"),
-                new Instruction("ANDI", "0b1001001000", "I"),
-                new Instruction("ANDIS", "0b1111001000", "I"),
-                new Instruction("ANDS", "0b1110101000", "R"),
-                new Instruction("B", "0b000101", "B"),
-                new Instruction("BR", "0b100101", "R"),
-                new Instruction("CBNZ", "0b10110101", "CB"),
-                new Instruction("CBZ", "0b10110100", "CB"),
-                new Instruction("DUMP", "0b11111111110", "Z"),
-                new Instruction("EOR", "0b11001010000", "R"),
-                new Instruction("EORI", "0b1101001000", "I"),
-                new Instruction("FADDD", "0b00011110011", "R"),
-                new Instruction("FADDS", "0b00011110001", "R"),
-                new Instruction("FCMPD", "0b00011110011", "R"),
-                new Instruction("FCMPS", "0b00011110001", "R"),
-                new Instruction("FDIVD", "0b00011110011", "R"),
-                new Instruction("FDIVS", "0b00011110001", "R"),
-                new Instruction("FMULD", "0b00011110011", "R"),
-                new Instruction("FMULS", "0b00011110001", "R"),
-                new Instruction("FSUBD", "0b00011110011", "R"),
-                new Instruction("FSUBS", "0b00011110001", "R"),
-                new Instruction("HALT", "0b11111111111", "Z"),
-                new Instruction("LDUR", "0b11111000010", "D"),
-                new Instruction("LDURB", "0b00111000010", "D"),
-                new Instruction("LDURD", "0b11111100010", "D"),
-                new Instruction("LDURH", "0b01111000010", "D"),
-                new Instruction("LDURS", "0b10111100010", "D"),
-                new Instruction("LDURSW", "0b10111000100", "D"),
-                new Instruction("LSL", "0b11010011011", "R"),
-                new Instruction("LSR", "0b11010011010", "R"),
-                new Instruction("MUL", "0b10011011000", "R"),
-                new Instruction("ORR", "0b10101010000", "R"),
-                new Instruction("ORRI", "0b1011001000", "I"),
-                new Instruction("PRNL", "0b11111111100", "Z"),
-                new Instruction("PRNT", "0b11111111101", "Z"),
-                new Instruction("SDIV", "0b10011010110", "R"),
-                new Instruction("SMULH", "0b10011011010", "R"),
-                new Instruction("STUR", "0b11111000000", "D"),
-                new Instruction("STURB", "0b00111000000", "D"),
-                new Instruction("STURD", "0b11111100000", "D"),
-                new Instruction("STURH", "0b01111000000", "D"),
-                new Instruction("STURS", "0b10111100000", "D"),
-                new Instruction("STURSW", "0b10111000000", "D"),
-                new Instruction("SUB", "0b11001011000", "R"),
-                new Instruction("SUBI", "0b1101000100", "I"),
-                new Instruction("SUBIS", "0b1111000100", "I"),
-                new Instruction("SUBS", "0b11101011000", "R"),
-                new Instruction("UDIV", "0b10011010110", "R"),
-                new Instruction("UMULH", "0b10011011110", "R")
+                new Instruction("ADD", "10001011000", "R"),
+                new Instruction("ADDI", "1001000100", "I"),
+                new Instruction("ADDIS", "1011000100", "I"),
+                new Instruction("ADDS", "10101011000", "R"),
+                new Instruction("AND", "10001010000", "R"),
+                new Instruction("ANDI", "1001001000", "I"),
+                new Instruction("ANDIS", "1111001000", "I"),
+                new Instruction("ANDS", "1110101000", "R"),
+                new Instruction("B", "000101", "B"),
+                new Instruction("BR", "100101", "R"), //TODO: BR instruction here doesn't 100% look like the one on the sheet
+                new Instruction("CBNZ", "10110101", "CB"),
+                new Instruction("CBZ", "10110100", "CB"), //TODO:
+                new Instruction("DUMP", "11111111110", "R"),
+                new Instruction("EOR", "11001010000", "R"),
+                new Instruction("EORI", "1101001000", "I"),
+                new Instruction("FADDD", "00011110011", "R"),
+                new Instruction("FADDS", "00011110001", "R"),
+                new Instruction("FCMPD", "00011110011", "R"),
+                new Instruction("FCMPS", "00011110001", "R"),
+                new Instruction("FDIVD", "00011110011", "R"),
+                new Instruction("FDIVS", "00011110001", "R"),
+                new Instruction("FMULD", "00011110011", "R"),
+                new Instruction("FMULS", "00011110001", "R"),
+                new Instruction("FSUBD", "00011110011", "R"),
+                new Instruction("FSUBS", "00011110001", "R"),
+                new Instruction("HALT", "11111111111", "R"),
+                new Instruction("LDUR", "11111000010", "D"),
+                new Instruction("LDURB", "00111000010", "D"),
+                new Instruction("LDURD", "11111100010", "D"),
+                new Instruction("LDURH", "01111000010", "D"),
+                new Instruction("LDURS", "10111100010", "D"),
+                new Instruction("LDURSW", "10111000100", "D"),
+                new Instruction("LSL", "11010011011", "R"),
+                new Instruction("LSR", "11010011010", "R"),
+                new Instruction("MUL", "10011011000", "R"),
+                new Instruction("ORR", "10101010000", "R"),
+                new Instruction("ORRI", "1011001000", "I"),
+                new Instruction("PRNL", "11111111100", "R"),
+                new Instruction("PRNT", "11111111101", "R"),
+                new Instruction("SDIV", "10011010110", "R"),
+                new Instruction("SMULH", "10011011010", "R"),
+                new Instruction("STUR", "11111000000", "D"),
+                new Instruction("STURB", "00111000000", "D"),
+                new Instruction("STURD", "11111100000", "D"),
+                new Instruction("STURH", "01111000000", "D"),
+                new Instruction("STURS", "10111100000", "D"),
+                new Instruction("STURSW", "10111000000", "D"),
+                new Instruction("SUB", "11001011000", "R"),
+                new Instruction("SUBI", "1101000100", "I"),
+                new Instruction("SUBIS", "1111000100", "I"),
+                new Instruction("SUBS", "11101011000", "R"),
+                new Instruction("UDIV", "10011010110", "R"),
+                new Instruction("UMULH", "10011011110", "R")
         };
 
         String DEV_FILEPATH = "C:/Users/Woodward/Documents/LEGv8-Disassembler/test.legv8asm.machine";
         boolean developerMode = true;
 
         if(args.length == 0 && !developerMode){
-            System.out.println("Hello I am a disassembler, please give me a file to disassemble!");
+            System.out.println("Hello I am a disassembler, please give me a file to disassemble :)");
         }
 
         try{
             //TODO: Change this to args[0] when handing in the project
-            File f = new File(DEV_FILEPATH);
-            FileInputStream fs = new FileInputStream(f);
+            FileInputStream fs = new FileInputStream(new File(DEV_FILEPATH));
             ArrayList<String> instructions = new ArrayList<String>();
 
+            //Reading in bytes and converting them into a binary string
+            //that will be used to interpret each instruction then
+            //stores those binary strings in a String[]
             while(fs.available() > 0){
                 byte[] bArr = fs.readNBytes(4);
                 int theInstructionInt = Byte.toUnsignedInt(bArr[0]);
+
+                theInstructionInt = theInstructionInt << 8;
                 theInstructionInt += Byte.toUnsignedInt(bArr[1]);
+
                 theInstructionInt = theInstructionInt << 8;
                 theInstructionInt += Byte.toUnsignedInt(bArr[2]);
+
                 theInstructionInt = theInstructionInt << 8;
                 theInstructionInt += Byte.toUnsignedInt(bArr[3]);
+
                 instructions.add(Integer.toBinaryString(theInstructionInt));
             }
 
             fs.close();
 
-            for(int i = 0; i < instructions.size(); i++){
-                String str = instructions.get(i);
-                String opcode = str.substring(0, 11);
-
+            for (String currentInstruction : instructions) {
                 //Figure out which instruction the opcode belongs to
-                int index = -1;
 
-                for(int k = 0; k < instructionSet.length; k++){
-                    if(instructionSet[k].opcodeMatch(opcode)){
-                        index = k;
-                        k = instructionSet.length + 5;
+                System.out.println(currentInstruction);
+
+                //Make sure the instruction is 32 bits wide
+                //This is to counter act Integer.toBinaryString() by adding back the leading zeros that it takes out
+                if(currentInstruction.length() < 32){
+                    String temp = "";
+
+                    for(int i = 0; i < (32-currentInstruction.length()); i++){
+                        temp += "0";
+                    }
+
+                    currentInstruction = temp + currentInstruction;
+                }
+
+                System.out.println(currentInstruction + "\n");
+
+                //Opcodes can be 6, 8, 9, 10, 11 bits wide
+                String o1 = currentInstruction.substring(0, 11);
+                String o2 = currentInstruction.substring(0, 10);
+                String o3 = currentInstruction.substring(0, 9);
+                String o4 = currentInstruction.substring(0, 8);
+                String o5 = currentInstruction.substring(0, 6);
+
+                int matchedIndex = -1;
+
+                //Check for all combinations of possible bit widths
+                for(int i = 0; i < instructionSet.length; i++){
+                    if(instructionSet[i].doesOpcodeMatch(o1) || instructionSet[i].doesOpcodeMatch(o2) || instructionSet[i].doesOpcodeMatch(o3) ||
+                       instructionSet[i].doesOpcodeMatch(o4) || instructionSet[i].doesOpcodeMatch(o5)){
+                        matchedIndex = i;
                     }
                 }
 
-                //Need to figure out which bits are apart of which parts of the instruction type
+                //Something went wrong
+                if(matchedIndex == -1){
+                    throw new NoSuchMethodException();
+                }
 
+                //At this point we know which instruction is being executed so we can call the method
+                //that breaks down the instruction in the Instruction class
+                String[] brokenDownInstruction = instructionSet[matchedIndex].breakInstruction(currentInstruction);
 
+                System.out.println(instructionSet[matchedIndex].getInstructionName());
+
+                for(String s : brokenDownInstruction){
+                    if(s != null){
+                        System.out.println(s);
+                    }
+                }
+
+                System.out.println("");
 
             }
-
-
+        }
+        catch(NoSuchMethodException nsme){
+            System.out.println("One or more instructions are invalid...");
         }
         catch(Exception e){
             System.out.println(e.getMessage());
